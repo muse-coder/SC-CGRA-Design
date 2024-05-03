438c2d431ab09d56ffc4f57087532e77d56499fe
muse-coder <935522618@qq.com>
2024-05-03 20:41:14 +0800

    Pantopkins算法完成

--- Software/Application/Pan_Tompkins_QRS_Detection/QRS_SC.py
+++ Software/Application/Pan_Tompkins_QRS_Detection/QRS_SC.py
@@ -6,6 +6,9 @@ import numpy as np
 class Pan_Tompkins_QRS_SC():
     seq1 = [0,16,24,8,12,28,20,4,6,22,30,14,10,26,18,2,3,19,27,11,15,31,23,7,5,21,29,13,9,25,17,1]
     seq2 = [0,16,8,24,12,28,4,20,10,26,2,18,6,22,14,30,15,31,7,23,3,19,11,27,5,21,13,29,9,25,1,17]
+    # seq1 = [0, 32, 48, 16, 24, 56, 40, 8, 12, 44, 60, 28, 20, 52, 36, 4, 6, 38, 54, 22, 30, 62, 46, 14, 10, 42, 58, 26, 18, 50, 34, 2, 3, 35, 51, 19, 27, 59, 43, 11, 15, 47, 63, 31, 23, 55, 39, 7, 5, 37, 53, 21, 29, 61, 45, 13, 9, 41, 57, 25, 17, 49, 33, 1]
+    # seq2 = [0, 32, 16, 48, 24, 56, 8, 40, 20, 52, 4, 36, 12, 44, 28, 60, 30, 62, 14, 46, 6, 38, 22, 54, 10, 42, 26, 58, 18, 50, 2, 34, 17, 49, 1, 33, 9, 41, 25, 57, 5, 37, 21, 53, 29, 61, 13, 45, 15, 47, 31, 63, 23, 55, 7, 39, 27, 59, 11, 43, 3, 35, 19, 51]
+
     validSegWidth = math.floor(math.log2(len(seq1)))+1
 
     # def quantize_SC(self ,num_1, num_2,scale_1 = 1,scale_2=1):
@@ -253,6 +256,258 @@ class Pan_Tompkins_QRS_SC():
 
         return bpass , der , sqr ,mwin
 
+class Pan_Tompkins_QRS_SC_64():
+    # seq1 = [0,16,24,8,12,28,20,4,6,22,30,14,10,26,18,2,3,19,27,11,15,31,23,7,5,21,29,13,9,25,17,1]
+    # seq2 = [0,16,8,24,12,28,4,20,10,26,2,18,6,22,14,30,15,31,7,23,3,19,11,27,5,21,13,29,9,25,1,17]
+    seq1 = [0, 32, 48, 16, 24, 56, 40, 8, 12, 44, 60, 28, 20, 52, 36, 4, 6, 38, 54, 22, 30, 62, 46, 14, 10, 42, 58, 26, 18, 50, 34, 2, 3, 35, 51, 19, 27, 59, 43, 11, 15, 47, 63, 31, 23, 55, 39, 7, 5, 37, 53, 21, 29, 61, 45, 13, 9, 41, 57, 25, 17, 49, 33, 1]
+    seq2 = [0, 32, 16, 48, 24, 56, 8, 40, 20, 52, 4, 36, 12, 44, 28, 60, 30, 62, 14, 46, 6, 38, 22, 54, 10, 42, 26, 58, 18, 50, 2, 34, 17, 49, 1, 33, 9, 41, 25, 57, 5, 37, 21, 53, 29, 61, 13, 45, 15, 47, 31, 63, 23, 55, 7, 39, 27, 59, 11, 43, 3, 35, 19, 51]
+
+    validSegWidth = math.floor(math.log2(len(seq1)))+1
+
+    # def quantize_SC(self ,num_1, num_2,scale_1 = 1,scale_2=1):
+    #     SC_num_1 = int(num_1*scale_1)
+    #     SC_num_2 = int(num_2*scale_2)
+    #     SC_num_1 = 65535 if(SC_num_1>=65536) else SC_num_1
+    #     SC_num_2 = 65535 if(SC_num_2>=65536) else SC_num_2
+    #     if(abs(SC_num_2)>=66636 or abs(SC_num_1) >=65536):
+    #         print(1)
+    #     # result[index] = (SC_tmp * annotation.fs) / 8 /65536
+    #     sign_1 = 1 if SC_num_1 > 0 else -1
+    #     sign_2 = 1 if SC_num_2 > 0 else -1
+    #     num_1 = abs(SC_num_1)
+    #     num_2 = abs(SC_num_2)
+    #     SC_result = scaled_mul(
+    #         num_1=num_1, num_2=num_2,
+    #         sobol_1=self.seq1, sobol_2=self.seq2, validSegWidth=self.validSegWidth,
+    #         sobolWidth=self.validSegWidth - 1, dataWidth=16
+    #     )
+    #     # exactRes =  (result[index] * annotation.fs) / 8
+    #     SCRes = SC_result * (sign_1 * sign_2)
+    #     return SCRes /( scale_1* scale_2)
+
+    def band_pass_filter(self, signal):
+        # Initialize result
+        result = None
+
+        # Create a copy of the input signal
+        sig = signal.copy()
+
+        # Apply the low pass filter using the equation given
+        # maxValue = np.max(signal)
+        # minValue = np.min(signal)
+        # scale =int ( 32768/( 2 ** math.ceil(math.log2(maxValue))))
+        for index in range(len(signal)):
+            sig[index] = signal[index]
+            if (index >= 1):
+                sig[index] += 2 * sig[index - 1]
+
+            if (index >= 2):
+                sig[index] -= sig[index - 2]
+
+            if (index >= 6):
+                sig[index] -= 2 * signal[index - 6]
+
+            if (index >= 12):
+                sig[index] += signal[index - 12]
+
+                # Copy the result of the low pass filter
+        result = sig.copy()
+        # sc_result = sig.copy()
+
+        # Apply the high pass filter using the equation given
+        for index in range(len(signal)):
+            result[index] = -1 * sig[index]
+            # sc_result[index] = -1 * sig[index]
+
+            if (index >= 1):
+                result[index] -= result[index - 1]
+                # sc_result[index] -= result[index - 1]
+
+            if (index >= 16):
+                exact_res= 32 * sig[index - 16]
+                # SC_tmp = scaled_mul(
+                # num_1=32, num_2=sig[index - 16],
+                # sobol_1=self.seq1, sobol_2=self.seq2, validSegWidth=self.validSegWidth,
+                # sobolWidth=self.validSegWidth-1, dataWidth=16
+                # )
+                # if(abs(SC_tmp/exact_res-1)>0.10):
+                #     print(1)
+                # sc_result[index] += SC_tmp
+                result[index] += exact_res
+            if (index >= 32):
+                result[index] += sig[index - 32]
+                # sc_result[index] += sig[index - 32]
+
+        # Normalize the result from the high pass filter
+        max_val = max(max(result), -min(result))
+        result = result / max_val
+        # sc_result = sc_result / max(max(sc_result), -min(sc_result))
+
+        return result
+
+    def derivative(self, signal,annotation):
+        '''
+        Derivative Filter
+        :param signal: input signal
+        :return: prcoessed signal
+
+        Methodology/Explaination:
+        The derivative of the input signal is taken to obtain the
+        information of the slope of the signal. Thus, the rate of change
+        of input is obtain in this step of the algorithm.
+
+        The derivative filter has the recursive equation:
+          y(nT) = [-x(nT - 2T) - 2x(nT - T) + 2x(nT + T) + x(nT + 2T)]/(8T)
+        '''
+
+        # Initialize result
+        result = signal.copy()
+
+        # Apply the derivative filter using the equation given
+        for index in range(len(signal)):
+            result[index] = 0
+
+            if (index >= 1):
+                exact_res= 2 * signal[index - 1]
+                # sc_res = self.quantize_SC(num_1=2 , num_2=signal[index - 1],scale_1=1,scale_2 = 65536 )
+                result[index] -= exact_res
+            if (index >= 2):
+                result[index] -= signal[index - 2]
+
+            if (index >= 2 and index <= len(signal) - 2):
+                # result[index] += 2 * signal[index + 1]
+                exact_res= 2 * signal[index + 1]
+                # sc_res = self.quantize_SC(num_1=2 , num_2=signal[index + 1],scale_2=65536 )
+                result[index] += exact_res
+            if (index >= 2 and index <= len(signal) - 3):
+                result[index] += signal[index + 2]
+
+            # tensorTmp = torch.tensor(result[index] ,device=self.device)
+            SC_tmp = math.floor(result[index] * 65536)
+            # result[index] = (SC_tmp * annotation.fs) / 8 /65536
+            sign_1 = 1 if SC_tmp>0 else -1
+            sign_2 = 1 if annotation.fs>0 else -1
+            num_1 = abs(SC_tmp)
+            num_2 = abs(annotation.fs)
+            SC_result = scaled_mul(
+            num_1=num_1, num_2=num_2,
+            sobol_1=self.seq1, sobol_2=self.seq2, validSegWidth=self.validSegWidth,
+            sobolWidth=self.validSegWidth-1, dataWidth=16
+            )
+            # exactRes =  (result[index] * annotation.fs) / 8
+            SCRes = SC_result / 8 / 65536 *(sign_1 * sign_2)
+            result[index] = SCRes
+        return result
+
+    def squaring(self, signal):
+        '''
+        Squaring the Signal
+        :param signal: input signal
+        :return: prcoessed signal
+
+        Methodology/Explaination:
+        The squaring process is used to intensify the slope of the
+        frequency response curve obtained in the derivative step. This
+        step helps in restricting false positives which may be caused
+        by T waves in the input signal.
+
+        The squaring filter has the recursive equation:
+          y(nT) = [x(nT)]^2
+        '''
+
+        # Initialize result
+        result = signal.copy()
+
+        # Apply the squaring using the equation given
+        for index in range(len(signal)):
+            # result[index] = signal[index] ** 2
+
+            quant_tmp = math.floor(result[index] * 1024)
+            num_1 = abs(quant_tmp)
+            SC_tmp = scaled_mul(
+                num_1=num_1, num_2=num_1,
+                sobol_1=self.seq1, sobol_2=self.seq2, validSegWidth=self.validSegWidth,
+                sobolWidth=self.validSegWidth - 1, dataWidth=16
+            )
+            # exactRes =  signal[index] ** 2
+            SCRes = SC_tmp / 1024 /1024
+            # if(abs(exactRes-SCRes)/exactRes) >0.2:
+            #     print(1)
+            result[index] = SCRes
+
+        return result
+
+    def moving_window_integration(self, signal , annotation):
+        '''
+        Moving Window Integrator
+        :param signal: input signal
+        :return: prcoessed signal
+
+        Methodology/Explaination:
+        The moving window integration process is done to obtain
+        information about both the slope and width of the QRS complex.
+        A window size of 0.15*(sample frequency) is used for more
+        accurate results.
+
+        The moving window integration has the recursive equation:
+          y(nT) = [y(nT - (N-1)T) + x(nT - (N-2)T) + ... + x(nT)]/N
+
+          where N is the number of samples in the width of integration
+          window.
+        '''
+
+        # Initialize result and window size for integration
+        result = signal.copy()
+        win_size = round(0.150 * annotation.fs)
+        sum = 0
+
+        # Calculate the sum for the first N terms
+        for j in range(win_size):
+            sum += signal[j] / win_size
+            result[j] = sum
+
+        # Apply the moving window integration using the equation given
+        for index in range(win_size, len(signal)):
+            sum += signal[index] / win_size
+            sum -= signal[index - win_size] / win_size
+            result[index] = sum
+
+        return result
+
+    def solve(self, signal,annotation):
+        '''
+        Solver, Combines all the above functions
+        :param signal: input signal
+        :return: prcoessed signal
+
+        Methodology/Explaination:
+        The peak detection algorithm works on the moving window and bandpass
+        filtered signal. So the input signal is first bandpassed, then the
+        output of the bandpass filter is given to the derivative function and
+        the result is squared. Finally the output of the squaring function
+        is given to the moving window integration function and returned.
+        '''
+
+        # Convert the input signal into numpy array
+        input_signal = signal.iloc[:, 1].to_numpy()
+
+        # Bandpass Filter
+        # global bpass
+        bpass = self.band_pass_filter(input_signal.copy())
+
+        # Derivative Function
+        # global der
+        der = self.derivative(bpass.copy() , annotation)
+
+        # Squaring Function
+        # global sqr
+        sqr = self.squaring(der.copy())
+
+        # Moving Window Integration Function
+        # global mwin
+        mwin = self.moving_window_integration(sqr.copy(), annotation)
+
+        return bpass , der , sqr ,mwin
 
 class Pan_Tompkins_QRS():
     def band_pass_filter(self, signal):
--- Software/Application/Pan_Tompkins_QRS_Detection/main.py
+++ Software/Application/Pan_Tompkins_QRS_Detection/main.py
@@ -15,14 +15,16 @@ def generateFigure(data_1,data_2,saveName , dpi ,dataLabel1, dataLabel2, xLabel 
     plt.figure(figsize=(20, 4), dpi=dpi)
     plt.xticks(np.arange(0, len(data_1) + 1, 150))
     start = 32 if title!='Moving Window Integrated Signal' else 100
-    plt.plot(data_1[start:len(data_1) - 2], color='blue', label=dataLabel1 ,linestyle='-', linewidth=3,)
-    plt.plot(data_2[start:len(data_2) - 2], color='red', label=dataLabel2 ,linestyle='--', linewidth=1, )
+    plt.plot(data_1[start:len(data_1) - 2], color='blue', label=dataLabel1 ,linestyle='solid', linewidth=2,)
+    plt.plot(data_2[start:len(data_2) - 2], color='red',  label=dataLabel2 ,linestyle='dashed', linewidth=1, )
+    # plt.plot(data_3[start:len(data_3) - 2], color='green', label=dataLabel3 ,linestyle='dotted', linewidth=2, )
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
     plt.title(title)
     plt.legend()
     plt.savefig(saveName)
     # plt.show()
+
 
 
 filename = f'./DataSet/{str(100 + signal_number)}'
@@ -32,10 +34,12 @@ annotation = wfdb.rdann(filename, 'atr', sampfrom=180, sampto=4000,shift_samps=T
 
 wfdb.plot_wfdb(record=record, annotation=annotation, time_units='seconds',figsize=(15,8))
 
-QRS_SC_detector = Pan_Tompkins_QRS_SC()
 ecg = pd.DataFrame(np.array([list(range(len(record.adc()))),record.adc()[:,0]]).T,columns=['TimeStamp','ecg'])
-SC_bpass , SC_der , SC_sqr ,SC_mwin  = QRS_SC_detector.solve(ecg,annotation)
+QRS_SC_detector_32 = Pan_Tompkins_QRS_SC()
+QRS_SC_detector_64 = Pan_Tompkins_QRS_SC_64()
 QRS_Exact_detector = Pan_Tompkins_QRS()
+SC_bpass_32 , SC_der_32 , SC_sqr_32 ,SC_mwin_32  = QRS_SC_detector_32.solve(ecg,annotation)
+SC_bpass_64 , SC_der_64 , SC_sqr_64 ,SC_mwin_64  = QRS_SC_detector_64.solve(ecg,annotation)
 Exact_bpass , Exact_der , Exact_sqr ,Exact_mwin  = QRS_Exact_detector.solve(ecg,annotation)
 
 # Plotting bandpassed signal
@@ -48,32 +52,47 @@ Exact_bpass , Exact_der , Exact_sqr ,Exact_mwin  = QRS_Exact_detector.solve(ecg,
 # plt.title("Bandpassed Signal")
 # plt.savefig('Bandpassed-Signal.png')
 # plt.show()
-generateFigure(data_1= SC_bpass ,data_2=Exact_bpass ,saveName='1.png' , dpi=300,dataLabel1= 'SC-Version' , dataLabel2= 'Exact-Version' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Bandpassed Signal')
-generateFigure(data_1= SC_der   ,data_2=Exact_der ,saveName='2.png' , dpi=300,dataLabel1= 'SC-Version' , dataLabel2= 'Exact-Version' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Derivative Signal')
-generateFigure(data_1= SC_sqr   ,data_2=Exact_sqr ,saveName='3.png' , dpi=300,dataLabel1= 'SC-Version' , dataLabel2= 'Exact-Version' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Squared Signal')
-generateFigure(data_1= SC_mwin  ,data_2=Exact_mwin ,saveName='4.png' , dpi=300,dataLabel1= 'SC-Version' , dataLabel2= 'Exact-Version' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Moving Window Integrated Signal')
+generateFigure(data_1= SC_bpass_64 ,data_2=Exact_bpass ,saveName='1.png' , dpi=300,dataLabel1= 'SC-CGRA' , dataLabel2= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Bandpassed Signal')
+generateFigure(data_1= SC_der_64   ,data_2=Exact_der ,saveName='2.png' , dpi=300,dataLabel1= 'SC-CGRA' , dataLabel2= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Derivative Signal')
+generateFigure(data_1= SC_sqr_64   ,data_2=Exact_sqr ,saveName='3.png' , dpi=300,dataLabel1= 'SC-CGRA' , dataLabel2= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Squared Signal')
+generateFigure(data_1= SC_mwin_64  ,data_2=Exact_mwin ,saveName='4.png' , dpi=300,dataLabel1= 'SC-CGRA' , dataLabel2= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Moving Window Integrated Signal')
+
+# generateFigure(data_1= SC_bpass_32 ,data_2= SC_bpass_64 ,data_3=Exact_bpass ,saveName='1.png' , dpi=300,dataLabel1= 'SC-CGRA' ,dataLabel2= 'SC-CGRA(bitstream extended)' , dataLabel3= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Bandpassed Signal')
+# generateFigure(data_1= SC_der_32   ,data_2= SC_der_64   ,data_3=Exact_der ,saveName='2.png' , dpi=300,  dataLabel1= 'SC-CGRA' ,dataLabel2= 'SC-CGRA(bitstream extended)' , dataLabel3= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Derivative Signal')
+# generateFigure(data_1= SC_sqr_32   ,data_2= SC_sqr_64   ,data_3=Exact_sqr ,saveName='3.png' , dpi=300,  dataLabel1= 'SC-CGRA' ,dataLabel2= 'SC-CGRA(bitstream extended)' , dataLabel3= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Squared Signal')
+# generateFigure(data_1= SC_mwin_32  ,data_2= SC_mwin_64  ,data_3=Exact_mwin ,saveName='4.png' , dpi=300, dataLabel1= 'SC-CGRA' ,dataLabel2= 'SC-CGRA(bitstream extended)' , dataLabel3= 'Typ-CGRA' , xLabel= 'Samples' ,yLabel= 'MLIImV',title='Moving Window Integrated Signal')
 
 
 # Convert ecg signal to numpy array
 signal = ecg.iloc[:,1].to_numpy()
 
 # Find the R peak locations
-hr = heart_rate_SC(signal=signal,samp_freq= annotation.fs,mwin=SC_mwin,bpass=SC_bpass )
-result = hr.find_r_peaks()
-result = np.array(result)
+hr_sc = heart_rate_SC(signal=signal,samp_freq= annotation.fs,mwin=SC_mwin_64,bpass=SC_bpass_64 )
+hr_exact = heart_rate_SC(signal=signal,samp_freq= annotation.fs,mwin=Exact_mwin,bpass=Exact_bpass )
+result_sc = hr_sc.find_r_peaks()
+result_sc = np.array(result_sc)
+
+result_exact = hr_exact.find_r_peaks()
+result_exact = np.array(result_exact)
+
 
 # Clip the x locations less than 0 (Learning Phase)
-result = result[result > 0]
+result_sc = result_sc[result_sc > 0]
+result_exact = result_exact[result_exact > 0]
 
 # Calculate the heart rate
-heartRate = (60*annotation.fs)/np.average(np.diff(result[1:]))
-print("Heart Rate",heartRate, "BPM")
+heartRate_sc = (60*annotation.fs)/np.average(np.diff(result_sc[1:]))
+heartRate_exact = (60*annotation.fs)/np.average(np.diff(result_exact[1:]))
+print("Heart Rate SC",heartRate_sc, "BPM")
+print("Heart Rate exact",heartRate_exact, "BPM")
 
 # Plotting the R peak locations in ECG signal
 plt.figure(figsize = (20,4), dpi = 300)
 plt.xticks(np.arange(0, len(signal)+1, 150))
 plt.plot(signal, color = 'blue')
-plt.scatter(result, signal[result], color = 'red', s = 50, marker= '*')
+plt.scatter(result_sc, signal[result_sc], label = "SC-CGRA",color = 'red', s = 50, marker= '*')
+plt.scatter(result_exact, signal[result_exact],label = "Typ-CGRA", color = 'blue', alpha=0.4, s = 50, marker= 'o')
+plt.legend()
 plt.xlabel('Samples')
 plt.ylabel('MLIImV')
 plt.title("R Peak Locations")
